29a54a157150e92a87ea27eec67ca506cca2cfc7
Sego <grayg@mail.ru>
2022-11-12 21:01:27 +0300

    lesson_3

--- authapp/views.py
+++ authapp/views.py
@@ -1,11 +1,22 @@
 from django.contrib.auth import get_user_model
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins, viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from .serializers import CustomUserModelSerializer
 
 User = get_user_model()
 
 
-class CustomUserViewSet(ModelViewSet):
+class CustomUserPaginations(PageNumberPagination):
+    page_size = 10
+
+
+class CustomUserViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = User.objects.all()
     serializer_class = CustomUserModelSerializer
+    pagination_class = CustomUserPaginations
--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # additional app
+    # plugins
     "corsheaders",
     "rest_framework",
+    "django_filters",
+    # custom app
     "authapp",
     "mainapp",
 ]
@@ -147,4 +149,7 @@ REST_FRAMEWORK = {
         "djangorestframework_camel_case.parser.CamelCaseJSONParser",
         # Any other parsers
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
