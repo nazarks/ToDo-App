deff0a2fdb944d3a4157daf103c4899424e5b959
Sego <grayg@mail.ru>
2022-11-19 23:49:43 +0300

    lesson_6

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 import authapp.views as auth_views
 import mainapp.views as mainapp_views
@@ -14,5 +16,8 @@ router.register("ToDos", mainapp_views.ToDoViewSet, basename="ToDo")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api-token-auth/", views.obtain_auth_token),
+    path("api-jwt-token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api-jwt-token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
