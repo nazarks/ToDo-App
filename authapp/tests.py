9786a433e3f972b195f7b97dd007f9f09362785f
Sego <grayg@mail.ru>
2022-11-26 00:59:43 +0300

    lesson_8

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from mixer.backend.django import mixer
+from rest_framework import status
 
-# Create your tests here.
+from .models import CustomUser
+from .views import CustomUserViewSet
+
+
+class TestCustomUserViewSet(TestCase):
+    def test_get_list_status_code(self):
+        factory = RequestFactory()
+        request = factory.get("/users/")
+        view = CustomUserViewSet.as_view({"get": "list"})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_list_data_count(self):
+        users_count = 10
+        mixer.cycle(users_count).blend(CustomUser)
+        factory = RequestFactory()
+        request = factory.get("/users/")
+        view = CustomUserViewSet.as_view({"get": "list"})
+        response = view(request)
+        self.assertEqual(len(response.data), users_count)
